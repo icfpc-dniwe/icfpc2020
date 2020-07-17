08bbfa1f0f5e43708c6bb9dc0e57b9aac2e57051
vuvko <notvuvko@gmail.com>
2020-07-17 21:12:27 +0300

    moved to bash

--- run.sh
+++ run.sh
@@ -1,3 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-/root/.local/bin/main "$@" || echo "run error code: $?"
+# /root/.local/bin/main "$@" || echo "run error code: $?"
+
+SERVER_URL=$1
+PLAYER_KEY=$2
+
+echo "ServerUrl: $SERVER_URL; PlayerKey: $PLAYER_KEY"
+
+RESPONSE_FILE=$( mktemp -t tmp.XXXXXXXXXX )
+HTTP_CODE=$( curl -s -o "$RESPONSE_FILE" -w '%{http_code}' -XPOST -d "1101000" "$SERVER_URL/aliens/send?apiKey=$PLAYER_KEY" )
+EXIT_CODE=$?
+
+if [[ ${EXIT_CODE} -ne 0 ]] ; then
+    echo "run error code: $EXIT_CODE"
+    exit 0
+fi
+
+if [[ ${HTTP_CODE} -ne 200 ]] ; then
+    echo "Unexpected server response:"
+    echo "HTTP code: $HTTP_CODE"
+    echo "Response body: $( cat "$RESPONSE_FILE" )"
+    exit 0
+fi
+
+echo "Server response: $( cat "$RESPONSE_FILE" )"
