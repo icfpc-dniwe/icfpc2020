2b683c12ad33df28028b2bdf6bd846a73ff328fc
Nikolay Amiantov <ab@fmap.me>
2020-07-18 02:15:56 +0300

    Revert unfinished code

--- src/ICFPC2020/AST.hs
+++ src/ICFPC2020/AST.hs
@@ -1,13 +1,7 @@
 module ICFPC2020.AST where
 
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HM
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Char8 as BS
-import Control.Monad.Reader
-
 data Token = TNumber !Int
-           | TMacro !ByteString
+           | TVariable !String
            | TOpenList
            | TComma
            | TCloseList
@@ -15,23 +9,21 @@ data Token = TNumber !Int
            deriving (Show, Eq)
 
 data Value = VNumber !Int
-           | VMacro !ByteString
+           | VVariable !String
            | VList ![Value]
            | VFunction !Function
            | VAp
-           | VApFun !Function
 
 type Declaration = (String, Value)
 
 instance Show Value where
   show (VNumber a) = show a
-  show (VFunction f) = BS.unpack $ funName f
-  show (VMacro v) = BS.unpack v
-  show (VList []) = "nil"
+  show (VFunction f) = funName f
+  show (VVariable v) = v
   show (VList vs) = show vs
   show VAp = "ap"
-  show (VApFun f) = "ap " ++ BS.unpack (funName f)
 
-data Function = Function { funName :: String
-                         , funApply :: !(Value -> [Value])
+data Function = Function { funName :: !String
+                         , funApply :: !(Value -> Maybe Value)
+                         , funRepresent :: [Value]
                          }
