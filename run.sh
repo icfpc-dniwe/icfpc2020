f31603e18ad253b97adfacc263e8e39e27e4fc34
alvearian <am-vi@yandex.ru>
2020-07-17 21:47:28 +0300

    again

--- run.sh
+++ run.sh
@@ -4,11 +4,12 @@
 
 SERVER_URL=$1
 PLAYER_KEY=$2
+API_KEY=46a9ed49d3414202898c86d9297976d9
 
 echo "ServerUrl: $SERVER_URL; PlayerKey: $PLAYER_KEY"
 
 RESPONSE_FILE=$( mktemp -t tmp.XXXXXXXXXX )
-HTTP_CODE=$( curl -s -o "$RESPONSE_FILE" -w '%{http_code}' -XPOST -H "accept: */*" -H "Content-Type: text/plain" -d "1101000" "$SERVER_URL/aliens/send?apiKey=$PLAYER_KEY" )
+HTTP_CODE=$( curl -s -o "$RESPONSE_FILE" -w '%{http_code}' -X POST "$SERVER_URL/aliens/send?apiKey=$API_KEY" -H "accept: */*" -H "Content-Type: text/plain" -d "1101000"  )
 EXIT_CODE=$?
 
 if [[ ${EXIT_CODE} -ne 0 ]] ; then
